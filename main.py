88c73eba4cb421adcae8f82fda22a66f3498718a
anmolsinghal <anmolsinghalpkl@gmail.com>
2016-08-21 10:52:32 +0530

    Update main.py

--- main.py
+++ main.py
@@ -9,13 +9,25 @@ from kivy.uix.label import Label
 from kivy.uix.progressbar import ProgressBar
 from plyer import gps
 
+rom kivy.clock import Clock
 
+from plyer import compass
+from plyer import accelerometer
 
 
 
 sm=ScreenManager()
 
 class RegScreen(Screen):
+	
+	def next(self):
+		sm.add_widget(RegScreen2(name='RegScreen2'))
+		sm.transition = SlideTransition(direction="left")
+		sm.current='RegScreen2'
+		#Starting Game Here
+		
+
+class RegScreen2(Screen):
 	
 	def register(self):
 		sm.add_widget(Dashboard(name='Dashboard'))
@@ -24,11 +36,6 @@ class RegScreen(Screen):
 		#Starting Game Here
 		sm.add_widget(Clue1(name='clue1'))
 		Dashboard.SolvedCount=0
-
-
-
-
-
 
 class Dashboard(Screen):
 	SolvedCount=NumericProperty()
@@ -73,7 +80,77 @@ class Clue(Screen):
 	gps.configure(on_location=distancefromclue)
 	gps.start()
 
-			
+
+
+class CompassTest(BoxLayout):
+    def __init__(self):
+        super(CompassTest, self).__init__()
+        self.sensorEnabled = False
+    def solved(self):
+    	sm.add_widget(Clue2(name='AccelerometerTest'))
+			self.Goto('AccelerometerTest',"left")    
+    def do_toggle(self):
+        try:
+            if not self.sensorEnabled:
+                compass.enable()
+                Clock.schedule_interval(self.get_readings, 1 / 20.)
+
+                self.sensorEnabled = True
+                self.ids.toggle_button.text = "Stop compass"
+            else:
+                compass.disable()
+                Clock.unschedule(self.get_readings)
+
+                self.sensorEnabled = False
+                self.ids.toggle_button.text = "Start compass"
+        except NotImplementedError:
+            import traceback
+            traceback.print_exc()
+            status = "Compass is not implemented for your platform"
+            self.ids.status.text = status
+
+    def get_readings(self, dt):
+        val = compass.orientation
+
+        self.ids.x_label.text = "X: " + str(val[0])
+        self.ids.y_label.text = "Y: " + str(val[1])
+        self.ids.z_label.text = "Z: " + str(val[2])
+
+class AccelerometerTest(BoxLayout):
+    def __init__(self):
+        super(AccelerometerTest, self).__init__()
+        self.sensorEnabled = False
+    def solved(self):
+    	sm.add_widget(Clue2(name='clue2'))
+			self.Goto('clue2',"left")     
+
+    def do_toggle(self):
+        try:
+            if not self.sensorEnabled:
+                accelerometer.enable()
+                Clock.schedule_interval(self.get_acceleration, 1 / 20.)
+
+                self.sensorEnabled = True
+                self.ids.toggle_button.text = "Stop Accelerometer"
+            else:
+                accelerometer.disable()
+                Clock.unschedule(self.get_acceleration)
+
+                self.sensorEnabled = False
+                self.ids.toggle_button.text = "Start Accelerometer"
+        except NotImplementedError:
+            import traceback
+            traceback.print_exc()
+            status = "Accelerometer is not implemented for your platform"
+            self.ids.accel_status.text = status
+
+    def get_acceleration(self, dt):
+        val = accelerometer.acceleration[:3]
+
+        if not val == (None, None, None):
+            self.ids.x_label.text = "X: " + str(val[0])
+            self.ids.y_label.text = "Y: " + str(val[1])
+            self.ids.z_label.text = "Z: " + str(val[2])			
 		
 
 screennames=[('Clue'.format(i)) for i in range(1,4)]
@@ -86,8 +163,8 @@ class Clue1(Clue):
 			self.IsSolved=True
 			Dashboard.SolvedCount+=1
 			print Dashboard.SolvedCount
-			sm.add_widget(Clue2(name='clue2'))
-			self.Goto('clue2',"left")
+			sm.add_widget(Clue2(name='CompassTest'))
+			self.Goto('CompassTest',"left")
 	
 class Clue2(Clue):
 	def Solved(self):
